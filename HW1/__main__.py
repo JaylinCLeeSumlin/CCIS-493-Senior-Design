91cfd9e91fba1443ce5c028dd12e3f2cdd548078
JaylinCLeeSumlin <jaylin92603@gmail.com>
2024-09-15 13:52:03 -0400

    feat: Check for user input. Prepare to parse client arguments. Add API base URL

--- HW1/__main__.py
+++ HW1/__main__.py
@@ -1,5 +1,14 @@
-def main():
-    pass
+import sys
+
+def main(args = None):
+    
+    if args is None:
+        args = sys.argv[1:]
+
+    parse_cli_argv(args)
+    
+def parse_cli_argv(argv):
+    base_url = "https://github.com/USEPA/RAQSAPI/"
 
 if __name__ == "__main__":
-    pass
+    main()
