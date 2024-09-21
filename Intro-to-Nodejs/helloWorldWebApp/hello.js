0f07ce55fea180cf0e507c36314e2cfbb300fc06
JaylinCLeeSumlin <jaylin92603@gmail.com>
2024-09-20 21:56:23 -0400

    Add comments to explain each line

--- Intro-to-Nodejs/helloWorldWebApp/hello.js
+++ Intro-to-Nodejs/helloWorldWebApp/hello.js
@@ -1,15 +1,19 @@
+// use require() to load http module and store returned HTTP instance in http module
 http = require('node:http');
 
+// executes when server gets request from client
+// processes incomign request and forms server response
 listener = function(request, response) {
-    response.writeHead(200, {'Content-Tpye': 'text/html'});
     // writeHead = send HTTP header
     // 200 = HTTP status
     // text/html = content type
+    response.writeHead(200, {'Content-Tpye': 'text/html'});
 
-    response.end('<h2>Hello World</h2>');
     // send response body as 'Hello World'
+    response.end('<h2 style="text-align: center;">Hello World</h2>');
 }
 
+// start server using createServer() and bind port 3000 using listen()
 server = http.createServer(listener);
 server.listen(3000);
 
